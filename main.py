SHIFT operation corrected. Fixed TQ, BQ and OQ calculation

main.py
@@ -39,7 +39,7 @@ def main(file: str):
     print(clustered_attr_matrix)
     print(f'Attribute order: {ordering}')
 
-    point = get_partition_point(clustered_attr_matrix, attr_usage_matrix, query_freq_matrix, query_cost_matrix)
+    point = get_partition_point(ordering, attr_usage_matrix, query_freq_matrix, query_cost_matrix)
 
     print(f'Split point without SHIFT operation is {point}')
 
@@ -49,10 +49,12 @@ def main(file: str):
     print(f'TA = {TA}')
     print(f'BA = {BA}')
 
-    shift, point = get_shifted_point(clustered_attr_matrix, attr_usage_matrix, query_freq_matrix, query_cost_matrix)
+    shift, point = get_shifted_point(ordering, attr_usage_matrix, query_freq_matrix, query_cost_matrix)
 
     print(f'Split point with SHIFT = {shift} operation is {point}')
 
+    # shift left by `-shift`
+    ordering = np.roll(ordering, -shift)
     TA = ordering[:point + 1]
     BA = ordering[point + 1:]
 

partition.py
@@ -1,40 +1,68 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 
-from matrix import left_shift, up_shift
+
+def get_query_set(attributes: np.ndarray, usage: np.ndarray, strict: bool = True) -> List[int]:
+    """
+    Find set of queries that access attributes
+    In strict mode, a given query must access all the attributes.
+    When strict=False, the query may access any attribute in attributes
+
+    :param attributes: attributes accessed by a query
+    :param usage: attribute usage matrix
+    :param strict: flag for specifying the type of constraint used
+    :return: list of queries accessing attributes
+    """
+    def constraint(index: int):
+        """
+        Defines the constraint for strict and non-strict mode
+
+        :param index: query being considered
+        :return: True if constraint met, False otherwise
+        """
+        if strict:
+            # usage has value 1 if a query is accessing that attribute
+            # we need the query to access only those attributes
+            # non-zero count in usage must be equal to number of attributes being considered
+            return np.count_nonzero(usage[index]) == attributes.shape[0]
+        else:
+            # the query may access any attribute in attributes
+            return True
+
+    num_query = usage.shape[0]
+    queries = []
+    for i in range(num_query):
+        flag = True
+        for attr in attributes:
+            # if ith query does not access attr, then ignore that query
+            if usage[i, attr] != 1:
+                flag = False
+                break
+        if flag and constraint(i):
+            queries.append(i)
+
+    return queries
 
 
-def z_cost(x: int, clustered: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> int:
+def z_cost(x: int, ordering: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> int:
     """
     Calculates cost for partition point x
 
     :param x: position to partition at
-    :param clustered: clustered attribute matrix
+    :param ordering: attribute ordering after clustering
     :param usage: attribute usage matrix
     :param freq: query frequency matrix
     :param cost: query cost matrix
     :return: cost of partitioning at x
     """
-    num_attr = clustered.shape[0]
     num_query, num_sites = freq.shape
-    AQ, TQ, BQ, OQ = [], [], [], []
+    TA = ordering[:x + 1]
+    BA = ordering[x + 1:]
 
-    for i in range(num_query):
-        row = []
-        for j in range(num_attr):
-            if usage[i, j] == 1:
-                row.append(j)
-        AQ.append(row)
-
-    start = num_attr - x
-    for i in range(num_query):
-        if AQ[i][1] <= start:
-            TQ.append(i)
-        elif AQ[i][0] > start:
-            BQ.append(i)
-        else:
-            OQ.append(i)
+    TQ = get_query_set(TA, usage)
+    BQ = get_query_set(BA, usage)
+    OQ = get_query_set(np.append(TA, BA), usage, False)
 
     CTQ, CBQ, COQ = 0, 0, 0
     for i in range(len(TQ)):
@@ -50,50 +78,46 @@ def z_cost(x: int, clustered: np.ndarray, usage: np.ndarray, freq: np.ndarray, c
     return (CTQ * CBQ) - (COQ ** 2)
 
 
-def get_partition_point(clustered: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> int:
+def get_partition_point(ordering: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> int:
     """
     Find the partition point in the clustered attribute matrix (2-way partition)
 
-    :param clustered: clustered attribute matrix
+    :param ordering: attribute order after clustering
     :param usage: attribute usage matrix
     :param freq: query frequency matrix
     :param cost: query cost matrix
     :return: index position of partition
     """
-    num_attr = clustered.shape[0]
+    num_attr = ordering.shape[0]
 
-    costs = np.asarray([z_cost(x, clustered, usage, freq, cost) for x in range(1, num_attr - 1)])
+    costs = np.asarray([z_cost(x, ordering, usage, freq, cost) for x in range(1, num_attr - 1)])
     return np.argmax(costs) + 1
 
 
-def get_shifted_point(clustered: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> Tuple[int, int]:
+def get_shifted_point(ordering: np.ndarray, usage: np.ndarray, freq: np.ndarray, cost: np.ndarray) -> Tuple[int, int]:
     """
     Calculate the optimal partition point (2-way partition) after applying SHIFT operations
 
-    :param clustered: clustered attribute matrix
+    :param ordering: attribute ordering after clustering
     :param usage: attribute usage matrix
     :param freq: query frequency matrix
     :param cost: query cost matrix
     :return: tuple with shift count and partition point
     """
-    num_attr = clustered.shape[0]
+    num_attr = ordering.shape[0]
     shift = 0
     point = 0
     best = -1_000_000
-    for i in range(num_attr):
-        for j in range(num_attr - 1, 0, -1):
-            z = z_cost(j, clustered, usage, freq, cost)
-            if z > best:
-                best = z
-                point = j
-                shift = i
-        clustered = left_shift(up_shift(clustered))
-        usage = left_shift(up_shift(usage))
 
-    for i in range(shift):
-        last = clustered[num_attr - 1]
-        for j in range(num_attr - 1, 1, -1):
-            clustered[j] = clustered[j - 1]
-        clustered[0] = last
+    # num_attr shifts can occur
+    for i in range(num_attr):
+        costs = np.asarray([z_cost(x, ordering, usage, freq, cost) for x in range(1, num_attr - 1)])
+        if (m := np.amax(costs)) > best:
+            best = m
+            point = np.argmax(costs) + 1
+            shift = i
+        # the following operation "rolls" (shifts the elements) by -1 (to the left)
+        # since ordering has one axis, it results in a simple left shift
+        ordering = np.roll(ordering, -1)
 
     return shift, point